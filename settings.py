288eab0d8a577a5c8c03aac19ecc60d12f76adf9
Christoph Heim <christoph.heim@env.ethz.ch>
2022-12-05 12:37:31 +0100

    Make environment file more generally valid.

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 description     Settings namelist for all routines in PGW for ERA5
-authors		Before 2022: original developments by Roman Brogli
+authors		    Before 2022: original developments by Roman Brogli
                 Since 2022:  upgrade to PGW for ERA5 by Christoph Heim 
 """
 ##############################################################################
